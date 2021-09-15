27a5ff721f6d90c3aba48f5585cf7c9cb10bc064
Dito YP <killcoder212@gmail.com>
2021-09-15 19:24:11 +0700

    add data processor for csv and yaml

--- src/Service/Data/DataProcessorService.php
+++ src/Service/Data/DataProcessorService.php
@@ -2,33 +2,84 @@
 
 namespace App\Service\Data;
 
-use App\Service\Registry\FolderRegistryService;
+use Symfony\Component\Yaml\Yaml;
 use App\Service\Requester\FetchDataService;
 use Symfony\Component\Filesystem\Filesystem;
+use App\Service\Registry\FolderRegistryService;
 use Symfony\Component\HttpKernel\KernelInterface;
 
 class DataProcessorService extends FolderRegistryService
 {
     private $fetchDataService;
+    private $dataManipulationService;
+    private $filesystem;
+    private $keys = [
+        "order_id",
+        "order_datetime",
+        "total_order_value",
+        "average_unit_price",
+        "distinct_unit_count",
+        "total_units_count",
+        "customer_state"
+    ];
+
     public function __construct(
         FetchDataService $fetchDataService,
-        KernelInterface $kernelInterface
+        KernelInterface $kernelInterface,
+        DataManipulationService $dataManipulationService,
+        Filesystem $filesystem
     ) {
         parent::__construct($kernelInterface);
         $this->fetchDataService = $fetchDataService;
+        $this->dataManipulationService = $dataManipulationService;
+        $this->filesystem = $filesystem;
+
+        if (!$this->filesystem->exists($this->getOutputPath())) $this->filesystem->mkdir($this->getOutputPath());
     }
 
     public function csv(string $src, bool $db = false)
     {
         $fetcher = $this->fetchDataService->fetch($src);
         $filename = $this->getFullPath($fetcher['filenameWithExtension']);
+        $outputFile = $this->getOutputPath($fetcher['filename'] . ".csv");
 
-        dump($filename);
+        // if ($this->filesystem->exists($outputFile)) {
+        //     return $outputFile;
+        // }
+
+        $fileToBeWritten = fopen($outputFile, "w+");
+
+        $csvItems = $this->dataManipulationService->getData($filename);
+
+        fputcsv($fileToBeWritten, $this->keys, ",");
+
+        foreach ($csvItems as $item) {
+            $last = end($item);
+            foreach ($item as $itm) {
+                fwrite($fileToBeWritten, "$itm");
+                if ($itm !== $last) {
+                    fwrite($fileToBeWritten, ",");
+                }
+            }
+            fwrite($fileToBeWritten, "\n");
+        }
+
+        return $outputFile;
     }
 
-    public function yaml(string $src)
+    public function yaml(string $src, bool $db = false)
     {
-        return "This gonna be YAML Side";
+        $fetcher = $this->fetchDataService->fetch($src);
+        $filename = $this->getFullPath($fetcher['filenameWithExtension']);
+        $outputFile = $this->getOutputPath($fetcher['filename'] . ".yaml");
+
+        $result = $this->dataManipulationService->getData($filename);
+
+        $yaml = Yaml::dump($result);
+
+        file_put_contents($outputFile, $yaml);
+
+        return $outputFile;
     }
 
     public function getFullPath(string $input)
