f6faf371c434c1ba8c7fbe5377056439baf1651f
Dito YP <killcoder212@gmail.com>
2021-09-15 17:35:46 +0700

    add official http client

--- src/Service/Requester/FetchDataService.php
+++ src/Service/Requester/FetchDataService.php
@@ -2,23 +2,31 @@
 
 namespace App\Service\Requester;
 
+use App\Service\Registry\FolderRegistryService;
 use Exception;
 use GuzzleHttp\Client;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\HttpClient\HttpClient;
 
-class FetchDataService
+class FetchDataService extends FolderRegistryService
 {
     public function fetch(string $src)
     {
-        $client = new Client([
-            'headers' => [],
-        ]);
+        $client = HttpClient::create();
 
-        $response = $client->get($src);
+        $response = $client->request('GET', $src);
 
         if ($response->getStatusCode() !== 200) throw new Exception('GTFO');
     }
 
     public function saveCache($input)
     {
+        $filesystem = new Filesystem();
+
+        if (!$filesystem->exists(sys_get_temp_dir() . "/inputs")) {
+            $filesystem->mkdir(sys_get_temp_dir() . "/inputs");
+        }
+
+        $filename = md5(uniqid());
     }
 }
