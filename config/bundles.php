d02606c13af901b8d0748bdd0bb482f8183ef31b
Dito YP <killcoder212@gmail.com>
2021-09-15 16:57:23 +0700

    add maker bundle

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,5 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Fidry\PsyshBundle\PsyshBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
