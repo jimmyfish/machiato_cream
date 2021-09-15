54e63de3955899011f6a807dbab9f70b6a18d570
Dito YP <killcoder212@gmail.com>
2021-09-15 18:09:30 +0700

    add data fetcher

--- src/Service/Requester/FetchDataService.php
+++ src/Service/Requester/FetchDataService.php
@@ -9,26 +9,41 @@ use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\HttpClient\HttpClient;
 use App\Service\Registry\FolderRegistryService;
 use Symfony\Component\HttpKernel\KernelInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 
 class FetchDataService extends FolderRegistryService
 {
     public function fetch(string $src)
-    {
-        $client = HttpClient::create();
-
-        $response = $client->request('GET', $src);
-
-        if ($response->getStatusCode() !== 200) throw new Exception('GTFO');
-    }
-
-    public function saveCache($input)
     {
         $filesystem = new Filesystem();
 
         if (!$filesystem->exists($this->getInputDir())) {
             $filesystem->mkdir($this->getInputDir());
         }
+        
+        $fileHandler = null;
 
-        $filename = md5(uniqid()) . ".jsonl";
+        try {
+            $client = HttpClient::create();
+
+            $response = $client->request('GET', $src);
+
+            if ($response->getStatusCode() !== 200) throw new Exception('GTFO');
+
+            $filename = md5(uniqid()) . ".jsonl";
+
+            $fileHandler = fopen($this->getInputDir() . "/" . $filename, "w+");
+
+            foreach ($client->stream($response) as $chunk) {
+                fwrite($fileHandler, $chunk->getContent());
+            }
+        } catch (TransportExceptionInterface $exception) {
+            return new JsonResponse(["message" => $exception->getMessage()]);
+        } catch (Exception $exception) {
+            return new JsonResponse(["message" => $exception->getMessage()]);
+        }
+
+        return $fileHandler;
     }
 }
