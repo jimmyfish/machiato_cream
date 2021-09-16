bc80a21247fbb49d5d4a52a0cd9c83c5d3c59add
Dito YP <killcoder212@gmail.com>
2021-09-16 11:06:27 +0700

    remove unn import

--- src/Command/MigrateDataCommand.php
+++ src/Command/MigrateDataCommand.php
@@ -3,11 +3,9 @@
 namespace App\Command;
 
 use App\Service\Data\DataProcessorService;
-use App\Service\Requester\FetchDataService;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
