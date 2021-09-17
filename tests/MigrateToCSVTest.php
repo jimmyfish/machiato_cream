c705583cccd26144eb105341d14df4ca15e2fc12
Dito YP <killcoder212@gmail.com>
2021-09-17 17:33:23 +0700

    Add yaml and db stored test

--- tests/MigrateToCSVTest.php
+++ tests/MigrateToCSVTest.php
@@ -2,6 +2,7 @@
 
 namespace App\Tests;
 
+use App\Entity\Order;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
@@ -14,17 +15,23 @@ class MigrateToCSVTest extends KernelTestCase
     /** @var EntityManagerInterface $entityManager */
     private $entityManager;
 
+    private $doctrine;
+
     /** @var Application $application */
     private $application;
 
     private $dataMigratorCommand;
     private $commandTester;
 
+    /**
+     * Rise and shine
+     */
     public function setUp(): void
     {
         $kernel = self::bootKernel();
 
         $this->entityManager = $kernel->getContainer()->get('doctrine')->getManager();
+        $this->doctrine = $kernel->getContainer()->get('doctrine');
         $this->application = new Application($kernel);
         $this->dataMigratorCommand = $this->application->find('data:migrate');
         $this->commandTester = new CommandTester($this->dataMigratorCommand);
@@ -54,17 +61,79 @@ class MigrateToCSVTest extends KernelTestCase
         $this->assertStringContainsString('OK', $output);
 
         # If file created and validated
-        $output = str_replace("\r", "", str_replace("\n", "", $output));
-        $directory = explode(" ", $output);
+        $output = preg_replace('/\s+/', '', $output);
+        $directory = explode("]", $output);
 
-        $this->assertFileExists($directory[2]);
+        $this->assertFileExists($directory[1]);
     }
 
+    /** @test migrate data to yaml */
+    public function get_data_and_convert_it_into_yaml(): void
+    {
+        $this->commandTester->execute([
+            'src' => $this->source,
+            '--filetype' => 'yaml'
+        ]);
+
+        $output = $this->commandTester->getDisplay();
+        $this->assertStringContainsString('OK', $output);
+
+        # If file created and validated
+        $output = preg_replace('/\s+/', '', $output);
+        $directory = explode("]", $output);
+
+        $this->assertFileExists($directory[1]);
+    }
+
+    /** @test convert into csv and store it into db */
     public function get_data_and_convert_it_into_csv_and_store_it_to_db(): void
     {
-        $this->assertTrue(true);
+        $this->commandTester->execute([
+            'src' => $this->source,
+            '--filetype' => 'csv',
+            '--db' => true,
+        ]);
+
+        $output = $this->commandTester->getDisplay();
+        $this->assertStringContainsString('OK', $output);
+
+        # If file created and validated
+        $output = preg_replace('/\s+/', '', $output);
+        $arrayOutput = explode(":", $output);
+
+        $hash = $arrayOutput[1];
+
+        $data = $this->entityManager->getRepository(Order::class)->countDataUsingHash($hash);
+
+        $this->assertGreaterThan(0, $data);
     }
 
+    /** @test migrate data to yaml */
+    public function get_data_and_convert_it_into_yaml_and_store_it_to_db(): void
+    {
+        $this->commandTester->execute([
+            'src' => $this->source,
+            '--filetype' => 'yaml',
+            '--db' => true,
+        ]);
+
+        $output = $this->commandTester->getDisplay();
+        $this->assertStringContainsString('OK', $output);
+
+        # If file created and validated
+        $output = preg_replace('/\s+/', '', $output);
+        $arrayOutput = explode(":", $output);
+
+        $hash = $arrayOutput[1];
+
+        $data = $this->entityManager->getRepository(Order::class)->countDataUsingHash($hash);
+
+        $this->assertGreaterThan(0, $data);
+    }
+
+    /**
+     * Cleaning and self-destruct
+     */
     public function tearDown(): void
     {
         parent::tearDown();
