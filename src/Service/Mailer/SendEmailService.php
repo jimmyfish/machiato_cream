fa9bce7bd55d6af881a61b320ba97d20e652550c
Dito YP <killcoder212@gmail.com>
2021-09-17 17:50:00 +0700

    Fix email service using vanilla mailer

--- src/Service/Mailer/SendEmailService.php
+++ src/Service/Mailer/SendEmailService.php
@@ -6,6 +6,7 @@ use Swift_Mailer;
 use Swift_Message;
 use Swift_Attachment;
 use Swift_SwiftException;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 use Symfony\Component\Mime\Email;
 use App\Service\Registry\FolderRegistryService;
 use Symfony\Component\Mailer\MailerInterface;
@@ -20,7 +21,8 @@ class SendEmailService
         Swift_Mailer $swiftMailer,
         FolderRegistryService $folderRegistryService,
         MailerInterface $mailer
-    ) {
+    )
+    {
         $this->swiftMailer = $swiftMailer;
         $this->mailer = $mailer;
         $this->folderRegistryService = $folderRegistryService;
@@ -28,39 +30,20 @@ class SendEmailService
 
     public function send($to, $subject = "", $carbon = [], $content = "", $attachment = null)
     {
+        $email = (new Email())
+            ->from("dito@tuta.io")
+            ->to($to)
+            ->subject($subject)
+            ->text("Hello there!")
+            ->attach(fopen($attachment, 'r'));
         try {
-            $fullPath = $this->folderRegistryService->getOutputPath($attachment);
-            $email = (new Email())
-                ->from("dito@tuta.io")
-                ->to($to)
-                ->subject($subject)
-                ->text("Hello there!")
-                ->attachFromPath($fullPath);
-
             $this->mailer->send($email);
 
             return true;
-        } catch (\Exception $exception) {
-            dump($exception->getMessage());
+        } catch (TransportExceptionInterface $e) {
+            dump($e->getMessage());
             return false;
-        }
-    }
-
-    public function swift($to, $subject = "", $carbon = [], $content = "", $attachment = null)
-    {
-        try {
-            $message = (new Swift_Message($subject))
-                ->setTo($to)
-                ->setBody("Hello world!");
-
-            $fullPath = $this->folderRegistryService->getOutputPath($attachment);
-
-            $message->attach(Swift_Attachment::fromPath($fullPath)->setFilename($attachment));
-
-            $this->swiftMailer->send($message);
-
-            return true;
-        } catch (Swift_SwiftException $exception) {
+        } catch (\Exception $exception) {
             dump($exception->getMessage());
             return false;
         }
